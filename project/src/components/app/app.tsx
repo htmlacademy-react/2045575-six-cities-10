e63624bfbe9162022916d5a1963ef32c81d1e706
Matvei <mateodenisous69@gmail.com>
2022-07-12 17:29:59 +0300

    Добавит марщрутизацию

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,4 +1,10 @@
+import { Route, Routes, BrowserRouter } from 'react-router-dom';
+import { AppRoute } from '../../const';
 import MainScreen from '../../pages/main-screen/main-screen';
+import LoginScreen from '../../pages/login-screen/login-screen';
+import FavoritesScreen from '../../pages/favorites-screen/favorites-screen';
+import PropertyScreen from '../../pages/property-screen/property-screen';
+import NotFoundScreen from '../../pages/not-found-screen/not-found-screen';
 
 type AppProps = {
   placeCardsCount: number;
@@ -6,8 +12,29 @@ type AppProps = {
 
 export default function App({placeCardsCount}: AppProps): JSX.Element {
   return (
-    <MainScreen
-      placeCardsCount={placeCardsCount}
-    />
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path={AppRoute.Main}
+          element={<MainScreen placeCardsCount={placeCardsCount}/>}
+        />
+        <Route
+          path={AppRoute.Login}
+          element={<LoginScreen />}
+        />
+        <Route
+          path={AppRoute.Favorites}
+          element={<FavoritesScreen />}
+        />
+        <Route
+          path={AppRoute.Room}
+          element={<PropertyScreen />}
+        />
+        <Route
+          path="*"
+          element={<NotFoundScreen />}
+        />
+      </Routes>
+    </BrowserRouter>
   );
 }
