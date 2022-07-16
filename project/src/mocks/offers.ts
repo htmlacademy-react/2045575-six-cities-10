ed8130c6031fd4d203ec948c87d87b4797d9101b
Matvei <mateodenisous69@gmail.com>
2022-07-16 16:10:10 +0300

    Создаст моки для оффера

---
+++ project/src/mocks/offers.ts
@@ -0,0 +1,156 @@
+import { Offer } from '../types/offer';
+
+export const offers: Offer[] = [
+  {
+    bedrooms: 3,
+    city: {
+      location: {
+        latitude: 52.370216,
+        longitude: 4.895168,
+        zoom: 10
+      },
+      name: 'Amsterdam'
+    },
+    description: 'A quiet cozy and picturesque that hides behind a river by the unique lightness of Amsterdam.',
+    goods: [
+      'Heating'
+    ],
+    host: {
+      avatarUrl: 'img/1.png',
+      id: 3,
+      isPro: true,
+      name: 'Angelina'
+    },
+    id: 1,
+    images: [
+      'img/1.png'
+    ],
+    isFavorite: true,
+    isPremium: false,
+    location: {
+      latitude: 52.35514938496378,
+      longitude: 4.673877537499948,
+      zoom: 8
+    },
+    maxAdults: 4,
+    previewImage: 'img/1.png',
+    price: 120,
+    rating: 4.8,
+    title: 'Beautiful & luxurious studio at great location',
+    type: 'apartment'
+  },
+  {
+    bedrooms: 3,
+    city: {
+      location: {
+        latitude: 52.370216,
+        longitude: 4.895168,
+        zoom: 10
+      },
+      name: 'Amsterdam'
+    },
+    description: 'A quiet Amsterdam.',
+    goods: [
+      'Heating'
+    ],
+    host: {
+      avatarUrl: 'img/1.png',
+      id: 3,
+      isPro: true,
+      name: 'Angelina'
+    },
+    id: 2,
+    images: [
+      'img/2.png'
+    ],
+    isFavorite: true,
+    isPremium: false,
+    location: {
+      latitude: 52.35514938496378,
+      longitude: 4.673877537499948,
+      zoom: 8
+    },
+    maxAdults: 4,
+    previewImage: 'img/2.png',
+    price: 240,
+    rating: 4.8,
+    title: 'Beautiful & luxurious studio at great location',
+    type: 'apartment'
+  },
+  {
+    bedrooms: 3,
+    city: {
+      location: {
+        latitude: 52.370216,
+        longitude: 4.895168,
+        zoom: 10
+      },
+      name: 'Amsterdam'
+    },
+    description: 'a river by the unique lightness.',
+    goods: [
+      'Heating'
+    ],
+    host: {
+      avatarUrl: 'img/1.png',
+      id: 3,
+      isPro: true,
+      name: 'Angelina'
+    },
+    id: 3,
+    images: [
+      'img/3.png'
+    ],
+    isFavorite: true,
+    isPremium: false,
+    location: {
+      latitude: 52.35514938496378,
+      longitude: 4.673877537499948,
+      zoom: 8
+    },
+    maxAdults: 4,
+    previewImage: 'img/3.png',
+    price: 360,
+    rating: 4.8,
+    title: 'Beautiful & luxurious studio at great location',
+    type: 'apartment'
+  },
+  {
+    bedrooms: 3,
+    city: {
+      location: {
+        latitude: 52.370216,
+        longitude: 4.895168,
+        zoom: 10
+      },
+      name: 'Amsterdam'
+    },
+    description: 'picturesque that hides behind.',
+    goods: [
+      'Heating'
+    ],
+    host: {
+      avatarUrl: 'img/1.png',
+      id: 3,
+      isPro: true,
+      name: 'Angelina'
+    },
+    id: 4,
+    images: [
+      'img/4.png'
+    ],
+    isFavorite: true,
+    isPremium: false,
+    location: {
+      latitude: 52.35514938496378,
+      longitude: 4.673877537499948,
+      zoom: 8
+    },
+    maxAdults: 4,
+    previewImage: 'img/4.png',
+    price: 480,
+    rating: 4.8,
+    title: 'Beautiful & luxurious studio at great location',
+    type: 'apartment'
+  }
+];
